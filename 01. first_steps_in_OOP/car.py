a45e89a1619b19d60622db00429dad6078ba1054
Ivo Todorov <ivo2291@gmail.com>
2023-03-03 18:57:58 +0200

    Add files via upload

---
+++ 01. first_steps_in_OOP/car.py
@@ -0,0 +1,15 @@
+class Car:
+    def __init__(self, name, model, engine):
+        self.name = name
+        self.model = model
+        self.engine = engine
+
+    def get_info(self):
+        return f"This is {self.name} {self.model} with engine {self.engine}"
+
+
+car = Car("Kia", "Rio", "1.3L B3 I4")
+print(car.get_info())
+
+my_car = Car("Audi", "A6", "3.0L TDI")
+print(my_car.get_info())

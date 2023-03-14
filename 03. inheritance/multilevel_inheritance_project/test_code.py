de46806f0d593703942f4bac365ba2669f566f7c
Ivo Todorov <ivo2291@gmail.com>
2023-03-14 14:00:51 +0200

    Add files via upload

---
+++ 03. inheritance/multilevel_inheritance_project/test_code.py
@@ -0,0 +1,11 @@
+from car import Car
+from sports_car import SportsCar
+
+car = Car()
+sports_car = SportsCar()
+
+print(car.move())
+print(car.drive())
+print(sports_car.move())
+print(sports_car.drive())
+print(sports_car.race())

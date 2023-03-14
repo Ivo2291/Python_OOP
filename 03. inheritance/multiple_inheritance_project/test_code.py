3278b752a5bf9f451aac7b6940f3ac6bdf8d131b
Ivo Todorov <ivo2291@gmail.com>
2023-03-14 13:58:01 +0200

    Add files via upload

---
+++ 03. inheritance/multiple_inheritance_project/test_code.py
@@ -0,0 +1,6 @@
+from inheritance.multiple_inheritance.project.teacher import Teacher
+
+new_teacher = Teacher()
+print(new_teacher.teach())
+print(new_teacher.sleep())
+print(new_teacher.get_fired())

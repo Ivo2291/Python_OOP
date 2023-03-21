caa8509e57e68a8b0878978074a16366fc3af67d
Ivo Todorov <ivo2291@gmail.com>
2023-03-21 17:16:18 +0200

    Add files via upload

---
+++ 04. encapsulation/exercise/football_team_generator/team.py
@@ -0,0 +1,26 @@
+from football_team_generator.player import Player
+
+
+class Team:
+    def __init__(self, name: str, rating: int):
+        self.__name = name
+        self.__rating = rating
+        self.__players = []
+
+    def add_player(self, player: Player):
+        if player not in self.__players:
+            self.__players.append(player)
+
+            return f"Player {player.name} joined team {self.__name}"
+
+        return f"Player {player.name} has already joined"
+
+    def remove_player(self, player_name: str):
+        try:
+            player_to_remove = next(filter(lambda p: p.name == player_name, self.__players))
+        except StopIteration:
+            return f"Player {player_name} not found"
+
+        self.__players.remove(player_to_remove)
+
+        return player_to_remove

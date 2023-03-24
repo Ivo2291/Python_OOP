0e833373e6dcae857acb82be408463fb97696cd6
Ivo Todorov <ivo2291@gmail.com>
2023-03-24 17:44:39 +0200

    Add files via upload

---
+++ 05. static_and_class_methods/movie_world/customer.py
@@ -0,0 +1,12 @@
+class Customer:
+    def __init__(self, name: str, age: int, id_num: int):
+        self.name = name
+        self.age = age
+        self.id = id_num
+        self.rented_dvds = []
+
+    def __repr__(self):
+        dvd_names = [dvd.name for dvd in self.rented_dvds]
+
+        return f"{self.id}: {self.name} of age {self.age}" \
+               f" has {len(self.rented_dvds)} rented DVD's ({', '.join(dvd_names)})"

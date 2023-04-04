ce7259955ee1827c3a592820595b29e1f8379c25
Ivo Todorov <ivo2291@gmail.com>
2023-04-04 17:02:57 +0300

    Add files via upload

---
+++ 06. polymorphism_and_abstraction/animals/kitten.py
@@ -0,0 +1,11 @@
+from animals.cat import Cat
+
+
+class Kitten(Cat):
+    def __init__(self, name: str, age: int):
+        super().__init__(name, age, gender="Female")
+
+        self.gender = "Female"
+
+    def make_sound(self):
+        return "Meow"

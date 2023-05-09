8e06389655db8f240532ec8c4a48b5dd187797c1
Ivo Todorov <ivo2291@gmail.com>
2023-05-09 17:36:40 +0300

    Add files via upload

---
+++ 09. decorators/exercise/computer_store/computer_store_app.py
@@ -0,0 +1,44 @@
+from computer_store.computer_types.desktop_computer import DesktopComputer
+from computer_store.computer_types.laptop import Laptop
+
+
+class ComputerStoreApp:
+    def __init__(self):
+        self.warehouse = []
+        self.profits = 0
+
+    def build_computer(self, type_computer: str, manufacturer: str, model: str, processor: str, ram: int):
+
+        if type_computer == "Desktop Computer":
+            computer = DesktopComputer(manufacturer, model)
+        elif type_computer == "Laptop":
+            computer = Laptop(manufacturer, model)
+
+        else:
+            raise ValueError(f"{type_computer} is not a valid type computer!")
+
+        computer.processor = processor
+        computer.ram = ram
+
+        self.warehouse.append(computer)
+
+        return computer.configure_computer(processor, ram)
+
+    def sell_computer(self, client_budget: int, wanted_processor: str, wanted_ram: int):
+        for computer in self.warehouse:
+            if all([
+                client_budget >= computer.price,
+                computer.processor == wanted_processor,
+                computer.ram >= wanted_ram
+            ]):
+                self.profits += client_budget - computer.price
+                self.warehouse.remove(computer)
+
+                return f"{computer} sold for {client_budget}$."
+
+        raise Exception("Sorry, we don't have a computer for you.")
+
+
+computer_store = ComputerStoreApp()
+print(computer_store.build_computer("Laptop", "Apple", "Macbook", "Apple M1 Pro", 64))
+print(computer_store.sell_computer(10000, "Apple M1 Pro", 32))

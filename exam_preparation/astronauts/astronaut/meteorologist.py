a4dfeadd5454df017a132fd3511e23cbeebf49f2
Ivo Todorov <ivo2291@gmail.com>
2023-05-26 16:17:53 +0300

    Add files via upload

---
+++ exam_preparation/astronauts/astronaut/meteorologist.py
@@ -0,0 +1,14 @@
+from astronauts.astronaut.astronaut import Astronaut
+
+
+class Meteorologist(Astronaut):
+    BREATHE_UNITS = 15
+
+    def __init__(self, name: str, oxygen: int = 90):
+        super().__init__(name, oxygen)
+
+    def breathe(self):
+        self.oxygen -= self.BREATHE_UNITS
+
+    def increase_oxygen(self, amount: int):
+        self.oxygen += amount

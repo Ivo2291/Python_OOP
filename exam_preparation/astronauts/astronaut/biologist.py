1816c9a76b32b68ec12e53066f9a86834121464b
Ivo Todorov <ivo2291@gmail.com>
2023-05-26 16:17:20 +0300

    Add files via upload

---
+++ exam_preparation/astronauts/astronaut/biologist.py
@@ -0,0 +1,14 @@
+from astronauts.astronaut.astronaut import Astronaut
+
+
+class Biologist(Astronaut):
+    BREATHE_UNITS = 5
+
+    def __init__(self, name: str, oxygen: int = 70):
+        super().__init__(name, oxygen)
+
+    def breathe(self):
+        self.oxygen -= self.BREATHE_UNITS
+
+    def increase_oxygen(self, amount: int):
+        self.oxygen += amount

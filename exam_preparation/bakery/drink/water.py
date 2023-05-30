5d1c9d17b312ef42df5e45dd3714f0c398c8ba0c
Ivo Todorov <ivo2291@gmail.com>
2023-05-30 16:38:44 +0300

    Add files via upload

---
+++ exam_preparation/bakery/drink/water.py
@@ -0,0 +1,9 @@
+from bakery.drink.drink import Drink
+
+
+class Water(Drink):
+    def __init__(self, name: str, portion: float, brand: str):
+        super().__init__(name, portion, 1.50, brand)
+
+    def __repr__(self):
+        return f" - {self.name} {self.brand} - {self.portion:.2f}ml - {self.price:.2f}lv"
